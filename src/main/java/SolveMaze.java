bcf4b4ed8c87ecb4fca4bb82c22ee9ebc07242f2
medhapatil <medhap3@illinois.edu>
2018-09-11 16:28:57 -0500

    lab 2 doneee

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,6 +34,14 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                while (!maze.canMove()) {
+                    maze.turnRight();
+                }
+                maze.move();
+            }
+
             // Implement your maze solving algorithm here
         }
 
